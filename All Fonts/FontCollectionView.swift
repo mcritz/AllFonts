882deb12c26fe8f0338b710f491d7b3b692bd127
Michael Critz <github@michaelcritz.com>
2018-04-28 08:50:24 -0700

    added tab view controller

---
+++ All Fonts/FontCollectionView.swift
@@ -0,0 +1,19 @@
+//
+//  FontCollectionView.swift
+//  All Fonts
+//
+//  Created by Michael Critz on 4/28/18.
+//  Copyright © 2018 Michael Critz. All rights reserved.
+//
+
+import Cocoa
+
+class FontCollectionView: NSCollectionView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
