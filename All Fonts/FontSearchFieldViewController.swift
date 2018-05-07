f5c3d30dc69bb55310b1c1d126532717f7bd0b84
Michael Critz <michael.critz@sony.com>
2018-05-07 13:49:23 -0700

    added view controller for fonts

---
+++ All Fonts/FontSearchFieldViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FontSearchFieldViewController.swift
+//  All Fonts
+//
+//  Created by Critz, Michael on 5/7/18.
+//  Copyright © 2018 Michael Critz. All rights reserved.
+//
+
+import Cocoa
+
+class FontSearchFieldViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ All Fonts/FontSearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FontSearchViewController.swift
+//  All Fonts
+//
+//  Created by Critz, Michael on 5/7/18.
+//  Copyright © 2018 Michael Critz. All rights reserved.
+//
+
+import Cocoa
+
+class FontSearchViewController: NSViewController {
+    @IBOutlet weak var fontSerachField: NSSearchField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+}
